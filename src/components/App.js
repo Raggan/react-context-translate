e736e9b2e3245939a418ac1c1d988e1c0c547781
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-21 11:18:59 +0200

    more setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import CreateUser from "./CreateUser";
 
 class App extends Component {
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <CreateUser />
+      </div>
+    );
   }
 }
 
