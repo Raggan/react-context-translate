eb02c4aea34059b7661f1e6d29d43bac39ddaab5
Simon Holdorf <simon.holdorf@gmail.com>
2019-07-21 11:30:41 +0200

    language select

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,25 @@ import React, { Component } from "react";
 import CreateUser from "./CreateUser";
 
 class App extends Component {
+  state = { language: "english" };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
   render() {
     return (
-      <div>
-        <CreateUser />
+      <div className="ui container">
+        <div>
+          Select language:{" "}
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag de"
+            onClick={() => this.onLanguageChange("german")}
+          />
+        </div>
       </div>
     );
   }
